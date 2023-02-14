464ce50352f656554673662ba1e2cf6bd41670b6
Artur <djajikamail@gmail.com>
2023-02-14 14:58:26 +0200

    second homework

---
+++ hw2/script.js
@@ -0,0 +1,24 @@
+function hello() {
+    let name = prompt('What is your name?').trim();
+    alert(`Hello, ${name}! How are you?`);
+}
+
+// hello();
+
+function showRow() {
+    let row1 = prompt('Введите данные').trim();
+    let row2 = prompt('Еще немного данных, пожалуйста').trim();
+    let row3 = prompt('И еще разок)').trim();
+    alert(row2 + ' ' + row3 + ' ' + row1);
+}
+
+// showRow();
+
+
+
+function printNumber() {
+    const num = prompt('Введите пятизначное число').trim();
+    alert(num.split('').join(' '));
+}
+
+printNumber();
