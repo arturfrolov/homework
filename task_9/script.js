7360f8c93821879a9027e33696f7a4c3bf36ca5f
Artur <djajikamail@gmail.com>
2023-03-06 13:43:18 +0200

    DOM, random image output. A recursive function of degree conversion.

--- task_9/script.js
+++ task_9/script.js
@@ -1,14 +1,3 @@
-// Вывод изображений
-// В папке images есть изображения 1.jpg, 2.jpg, 3.jpg, 4.jpg, 5 .jpg, 6.jpg, 7.jpg, 8.jpg, 9.jpg 
-// Вывести изображение из этой папки, полученное случайным образом ( Math.random)
-
-//   Рекурсивный свод в степень
-// Реализовать рекурсивную функцию, сводящую число в степень.
-// Число, которое следует преподнести к степени, передается как первый аргумент в функции;
-// Степень передается как второй аргумент в функцию pow (num, degree).
-
-
-
 const img = document.querySelector('.random_img');
 const btn = document.querySelector('.genere_img');
 
@@ -34,4 +23,4 @@ function pow (num, degree) {
     return num * pow(num, degree - 1);
 }
 
-console.log(pow(2, 3));
+console.log(pow(2, 4));
