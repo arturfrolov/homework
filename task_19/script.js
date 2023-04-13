3aae78b535ba6f41c21b679f3a7f8303f671cd00
Artur <djajikamail@gmail.com>
2023-04-13 19:13:25 +0300

    Task - 19. Student constructor.

--- task_19/script.js
+++ task_19/script.js
@@ -1,34 +1,14 @@
-// Вам нужно сделать конструктор сущности "Студент".
-// У студента есть имя, фамилия, год рождения — это свойства. Есть массив с оценками, это тоже свойство. 
-// И есть возможность получить возраст студента и его средний балл – это методы.
-
-// Еще у всех студентов есть по массиву одинаковой длины, в нем 25 элементов, 
-// сначала он не заполнен, но на 25 элементов. Это массив, в котором отмечается посещаемость, 
-// всякий раз, когда мы вызываем метод .present() на очередное пустое место, 
-// в массив записывается true, когда вызываем .absent() - записывается false. 
-// Предусмотрите любую защиту от того, чтобы в массиве посещаемости не могло быть больше 25 записей. 
-// Массив – это свойство, present и absent – методы.
-
-// Последний метод: .summary(), проверяет среднюю оценку и среднее посещение(количество Посещений/количествоЗанятий), 
-// и если средняя оценка больше 90, а среднее посещение больше 0.9, 
-// то метод summary возвращает строку "Молодец!", если одно из этих значений меньше , 
-// то - "Хорошо, но можно лучше", если оба ниже - "Редиска!".
-// Не забудьте после того, как напишите этот конструктор, 
-// создать 2-3 экземпляра (конкретных студентов) и показать использование этих методов.
-
 function Student({ name, surname, birthYear, grades }) {
     this.name = name;
     this.surname = surname;
     this.birthYear = birthYear;
     this.grades = grades;
-
     this.attendance = new Array(25);
-    const date = new Date();
-    const year = date.getFullYear();
     let index = 0;
 
     this.age = function () {
-        return year - birthYear;
+        const currentYear = new Date().getFullYear();
+        return currentYear - birthYear;
     };
 
     this.averageGrade = function () {
@@ -44,6 +24,7 @@ function Student({ name, surname, birthYear, grades }) {
             this.attendance[index] = true;
             index++;
         }
+        return this;
     };
 
     this.absent = function () {
@@ -51,14 +32,15 @@ function Student({ name, surname, birthYear, grades }) {
             this.attendance[index] = false;
             index++;
         }
+        return this;
     };
 
     this.summary = function () {
         const averageGrade = this.averageGrade();
-        const numberOfVisits = this.attendance.filter(item => {
+        const presenceInClass = this.attendance.filter(item => {
             return item === true;
         });
-        const averageVisits = numberOfVisits.length / this.attendance.length;
+        const averageVisits = presenceInClass.length / this.attendance.length;
         if ((averageGrade >= 90) && (averageVisits >= 0.9)) {
             return 'Молодец!';
         } else if ((averageGrade < 90) && (averageVisits < 0.9)) {
@@ -69,44 +51,119 @@ function Student({ name, surname, birthYear, grades }) {
     };
 }
 
-const vasya = new Student({
-    name: 'Vasily',
-    surname: 'Petrov',
-    birthYear: 1998,
-    grades: [56, 58, 99, 81, 51, 67, 88, 53, 89, 81, 81, 77, 68, 82, 99, 57, 100, 72, 59, 92, 50, 76, 63, 59, 93],
+document.querySelector('.Student1').addEventListener('click', () => {
+    const vasya = new Student({
+        name: 'Vasily',
+        surname: 'Petrov',
+        birthYear: 1998,
+        grades: [56, 58, 99, 81, 51, 67, 88, 53, 89, 81, 81, 77, 68, 82, 99, 57, 100, 72, 59, 92, 50, 76, 63, 59, 93],
+    });
+    vasya
+        .present()
+        .absent()
+        .present()
+        .present()
+        .present()
+        .absent()
+        .present()
+        .present()
+        .present()
+        .present()
+        .absent()
+        .present()
+        .present()
+        .absent()
+        .present()
+        .present()
+        .absent()
+        .present()
+        .absent()
+        .present()
+        .present()
+        .present()
+        .absent()
+        .absent()
+        .present();
+
+    console.log('Age -', vasya.age());
+    console.log('Average Grade -', vasya.averageGrade());
+    console.log('Summary -', vasya.summary());
 });
-vasya.present();
-vasya.absent();
-vasya.present();
-vasya.present();
-vasya.present();
-vasya.absent();
-vasya.absent();
-vasya.present();
-vasya.present();
-vasya.present();
-vasya.absent();
-vasya.present();
-vasya.present();
-vasya.present();
-vasya.present();
-vasya.present();
-vasya.absent();
-vasya.absent();
-vasya.present();
-vasya.present();
-vasya.present();
-vasya.absent();
-vasya.absent();
-vasya.present();
-vasya.present();
-vasya.present();
 
-// console.log(vasya.age());
-// console.log(vasya.averageGrade());
-// console.log(vasya);
-console.log(vasya.summary());
+document.querySelector('.Student2').addEventListener('click', () => {
+    const petro = new Student({
+        name: 'Petr',
+        surname: 'Ivanov',
+        birthYear: 2002,
+        grades: [86, 88, 100, 89, 90, 90, 87, 91, 93, 98, 92, 93, 97, 92, 89, 92, 85, 100, 91, 94, 85, 86, 89, 85, 100],
+    });
+    petro
+        .present()
+        .present()
+        .present()
+        .present()
+        .absent()
+        .present()
+        .present()
+        .present()
+        .present()
+        .present()
+        .present()
+        .present()
+        .present()
+        .present()
+        .present()
+        .present()
+        .present()
+        .present()
+        .present()
+        .present()
+        .present()
+        .present()
+        .absent()
+        .present()
+        .present();
+
+    console.log('Age -', petro.age());
+    console.log('Average Grade -', petro.averageGrade());
+    console.log('Summary -', petro.summary());
+});
 
 
-const arr = Array.from({length: 25}, () => Math.floor(Math.random() * 51) + 50);
-console.log(arr);
+document.querySelector('.Student3').addEventListener('click', () => {
+    const sergey = new Student({
+        name: 'Sergey',
+        surname: 'Gorelov',
+        birthYear: 1995,
+        grades: [92, 96, 87, 100, 86, 91, 99, 95, 88, 92, 88, 87, 87, 95, 85, 96, 91, 99, 86, 93, 97, 100, 92, 85, 98],
+});
+    sergey
+        .present()
+        .present()
+        .present()
+        .present()
+        .present()
+        .absent()
+        .present()
+        .present()
+        .present()
+        .present()
+        .present()
+        .present()
+        .present()
+        .present()
+        .present()
+        .present()
+        .absent()
+        .present()
+        .present()
+        .present()
+        .present()
+        .absent()
+        .present()
+        .present()
+        .present();
+    console.log('Age -',sergey.age());
+    console.log('Average Grade -', sergey.averageGrade());
+    console.log('Summary -', sergey.summary());
+});
